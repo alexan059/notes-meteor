06d910d3a15336e697e1ff36b7d17870e33fe297
Alexander <accounts.alexan059@gmail.com>
2017-05-11 18:20:37 +0200

    Mark selected note in list

--- imports/ui/NoteListItem.js
+++ imports/ui/NoteListItem.js
@@ -8,6 +8,7 @@ export const NoteListItem = (props) => {
     return (
         <div onClick={() => props.Session.set('selectedNoteId', props.note._id)}>
             <h5>{props.note.title || 'Untitled note'}</h5>
+            {props.note.selected ? 'selected' : undefined}
             <p>{momemt(props.note.updatedAt).format('M/DD/YY')}</p>
         </div>
     );
